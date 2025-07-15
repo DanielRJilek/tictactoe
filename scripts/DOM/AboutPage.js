81a4de0e884a6c36ca31456f0d4d083575b3ed17
Daniel <jilek.d@northeastern.edu>
2025-07-15 19:59:26 -0400

    Beginnings of about page.

--- scripts/DOM/AboutPage.js
+++ scripts/DOM/AboutPage.js
@@ -4,19 +4,39 @@ class AboutPage {
         this.container = container;
     }
 
-    createButtons(title_page) {
+    createButtons(about_page) {
         const back = document.createElement("button");
         back.textContent = "Back";
         back.setAttribute("id", "back-button");
         back.addEventListener("click", this.view.renderTitlePage.bind(this.view));
-        title_page.appendChild(back);
+        about_page.appendChild(back);
+    }
+
+    createContent(about_page) {
+        const content = document.createElement("div");
+        content.setAttribute("id", "about-content");
+        const slideshow = document.createElement("div");
+        slideshow.classList.add("slideshow");
+
+        const slide1 = document.createElement("div");
+        slide1.classList.add("slide", "fade");
+        slide1.style.backgroundImage = "url('../images/Alfonso_X_el_Sabio_y_su_corte.jpg')"
+        const caption1 = document.createElement("div");
+        caption1.classList.add("caption");
+        slide1.appendChild(caption1)
+        slideshow.appendChild(slide1);
+
+        content.appendChild(slideshow);
+        
+        about_page.appendChild(content);
     }
 
     render() {
-        const title_page = document.createElement("div");
-        title_page.setAttribute("id", "title-page");
-        this.createButtons(title_page);
-        this.container.appendChild(title_page);
+        const about_page = document.createElement("div");
+        about_page.setAttribute("id", "about-page");
+        this.createContent(about_page);
+        this.createButtons(about_page);
+        this.container.appendChild(about_page);
     }
 }
 
