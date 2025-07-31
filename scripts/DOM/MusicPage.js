76c91176086603c9b9c9b171df40d81ea1cab6ce
Daniel <jilek.d@northeastern.edu>
2025-07-31 15:38:23 -0400

    Updates to music page.

--- scripts/DOM/MusicPage.js
+++ scripts/DOM/MusicPage.js
@@ -4,17 +4,50 @@ class MusicPage {
         this.container = container;
     }
 
-    render() {
-        const music_page = document.createElement("div");
-        music_page.setAttribute("id", "music-page");
-
+    createButtons(music_page) {
         const back = document.createElement("button");
         back.textContent = "Back";
         back.setAttribute("id", "back-button");
         // replace bind with arrow function????
         back.addEventListener("click", this.view.renderTitlePage.bind(this.view));
         music_page.appendChild(back);
+    }
+
+    async getFileContent(file) {
+        let x = await fetch(file);
+        let y = await x.text();
+        return y;
+    }
+
+    async createAboutText(content) {
+        let text = await this.getFileContent("./images/music.txt");
+        const aboutText = document.createElement("div");
+        aboutText.setAttribute("id", "about-text");
+        aboutText.textContent = text;
+        content.appendChild(aboutText);
+    }
+
+    async createContent(music_page) {
+        const content = document.createElement("div");
+        content.setAttribute("id", "about-content");
+        // await this.createSlides(content);
+        const title = document.createElement("title");
+        title.textContent = "Cantigas de Santa Maria";
+        await this.createAboutText(content);
+        music_page.appendChild(content);
+    }
+
+    async render() {
+        const music_page = document.createElement("div");
+        music_page.setAttribute("id", "music-page");
+        await this.createContent(music_page);
+        this.createButtons(music_page);
         this.container.appendChild(music_page);
+        if (this.view.audio_playing == false) {
+            let audio = new Audio('./audio/cantigas.mp3')
+            audio.play();
+            this.view.audio_playing = true;
+        }
     }
 }
 
